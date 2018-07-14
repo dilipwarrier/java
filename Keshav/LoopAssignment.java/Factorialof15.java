377f3e192b1c82512ba31f7c143f27c0e9539e1e
Kesh <35199442+keshthegod@users.noreply.github.com>
2018-07-14 11:05:11 -0700

    First and Second Assignments

---
+++ Keshav/LoopAssignment.java/Factorialof15.java
@@ -0,0 +1,14 @@
+class Factorialof15
+{
+  public static void main( String [] args)
+{
+  int i;
+  double fact=1;
+  int number=15;
+  for(i=1;i<=number;i++){
+    fact=fact*i;
+}
+  System.out.println("Factorial of 15 is:");
+  System.out.println(fact);
+}
+}
