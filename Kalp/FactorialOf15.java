ed0704476cdd9d2c3216bd568c2594222b1ccb83
Kalp Soni <kalpsoni220@gmail.com>
2018-08-31 18:13:22 -0400

    Assignment #5

--- Kalp/FactorialOf15.java
+++ Kalp/FactorialOf15.java
@@ -2,11 +2,11 @@ class FactorialOf15{
     public static void main(String args[]){
       int i;
       double fact=1;
-      int number=15;//It is the number to calculate factorial.
+      int number= variable x;//It is the number to calculate factorial.
     for(i=1;i<=number;i++){
     fact=fact*i;
     }
-    System.out.println("Factorial of 15 is:");
+    System.out.println("Factorial of variable x is:");
     System.out.println(fact);
 
   }
